a7181d95d789a957717b125161ef32a1ae9cc869
yh <azss0470@gmail.com>
2023-11-21 10:53:09 +0900

    2번 대소문자 변환 풀이

--- 1주차/오예환/대소문자변환.java
+++ 1주차/오예환/대소문자변환.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class 대소문자변환 {
+
+    public static String solution(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] cArr = str.toCharArray();
+
+        for (char c : cArr) {
+            if (65 <= c && c <= 90) {
+                stringBuilder.append((char)(c + 32));
+            } else if (97 <= c && c <= 122) {
+                stringBuilder.append((char)(c - 32));
+            }
+        }
+        return stringBuilder.toString();
+
+    }
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(solution(str));
+    }
 }
