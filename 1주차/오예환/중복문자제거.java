33ec788f3ddea912e2522cae84252057e05b7d50
yh <azss0470@gmail.com>
2023-11-21 16:19:28 +0900

    -

--- 1주차/오예환/중복문자제거.java
+++ 1주차/오예환/중복문자제거.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class 중복문자제거 {
+import java.util.Scanner;
+
+public class 중복문자제거 {
+    public static String solution(String str) {
+        int[] n = new int[26];
+        char[] charArr = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char c : charArr) {
+            if (n[(int)c - 97] == 0) {
+                stringBuilder.append(c);
+                n[(int)c - 97] = 1;
+            }
+        }
+
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(solution(str));
+    }
 }
